5c701e79633f1f0bfbef3331426eadbd144fbfea
Marcus <6180175+MKindberg@users.noreply.github.com>
2024-10-20 20:47:09 +0200

    Use tree-sitter queries to get data

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,72 @@
 use std::collections::hash_map::HashMap;
 use std::fs;
+use streaming_iterator::StreamingIterator;
 use tree_sitter as TS;
 
-#[derive(Debug, PartialEq, Eq, Hash)]
-enum Language {
-    Rust,
-    Other,
-}
-
-impl Language {
-    fn new(filename: &str) -> Self {
-        if filename.ends_with(".rs") {
-            return Self::Rust;
-        }
-        return Self::Other;
+trait Language {
+    fn matches_filename(&self, filename: &str) -> bool;
+    fn name(&self) -> &str;
+    fn language(&self) -> Option<TS::Language> {
+        None
     }
-
-    fn set_language(&self, parser: &mut TS::Parser) {
-        match self {
-            Self::Rust => parser
-                .set_language(&tree_sitter_rust::language())
-                .expect("Error loading Rust grammar"),
-            Self::Other => (),
-        }
+    fn loop_query(&self) -> Option<&str> {
+        None
+    }
+    fn function_query(&self) -> Option<&str> {
+        None
+    }
+    fn variable_query(&self) -> Option<&str> {
+        None
     }
 }
-impl Default for Language {
-    fn default() -> Self {
-        Self::Other
+
+struct Rust {}
+impl Language for Rust {
+    fn name(&self) -> &str {
+        "Rust"
+    }
+    fn matches_filename(&self, filename: &str) -> bool {
+        filename.ends_with(".rs")
+    }
+    fn language(&self) -> Option<TS::Language> {
+        Some(tree_sitter_rust::LANGUAGE.into())
+    }
+    fn loop_query(&self) -> Option<&str> {
+        Some(
+            "
+(for_expression)
+(while_expression)
+(loop_expression)
+",
+        )
+    }
+    fn function_query(&self) -> Option<&str> {
+        Some("(function_item)")
+    }
+    fn variable_query(&self) -> Option<&str> {
+        Some("(let_declaration)")
+    }
+}
+
+struct Other {}
+impl Language for Other {
+    fn name(&self) -> &str {
+        "Other"
+    }
+    fn matches_filename(&self, filename: &str) -> bool {
+        filename.ends_with(".rs")
+    }
+    fn language(&self) -> Option<TS::Language> {
+        None
+    }
+    fn loop_query(&self) -> Option<&str> {
+        None
+    }
+    fn function_query(&self) -> Option<&str> {
+        None
+    }
+    fn variable_query(&self) -> Option<&str> {
+        None
     }
 }
 
@@ -38,38 +77,47 @@ struct Stats {
     functions: usize,
     variables: usize,
     loops: usize,
-    language: Language,
 }
 
 impl Stats {
-    fn new(filename: &str) -> Self {
-        let mut stats = Self::default();
-        stats.language = Language::new(filename);
+    fn new() -> Self {
+        let stats = Self::default();
 
-        stats.update(filename);
         return stats;
     }
 
-    fn update(&mut self, filename: &str) {
+    fn update(&mut self, filename: &str, language: &Box<dyn Language>) {
         let content = fs::read_to_string(filename).unwrap();
 
-        let mut parser = TS::Parser::new();
-        self.language.set_language(&mut parser);
+        if let Some(lang) = language.language() {
+            let mut parser = TS::Parser::new();
+            parser.set_language(&lang).unwrap();
 
-        let tree = parser.parse(&content, None).unwrap();
-        let root_node = tree.root_node();
+            let tree = parser.parse(&content, None).unwrap();
+            let root_node = tree.root_node();
 
-        let mut cursor = root_node.walk();
+            let mut query_cursor = TS::QueryCursor::new();
 
-        while let Some(node) = Self::next_node(&mut cursor) {
-            if node.kind() == "function_item" {
-                self.functions += 1;
-            } else if node.kind() == "let" {
-                self.variables += 1;
-            } else if node.kind() == "for_expression" || node.kind() == "loop_expression" || node.kind() == "while_expression" {
-                self.loops += 1;
+            if let Some(query) = language.function_query() {
+                let function_query = TS::Query::new(&parser.language().unwrap(), query).unwrap();
+                self.functions = query_cursor
+                    .matches(&function_query, root_node, content.as_bytes())
+                    .count();
+            }
+            if let Some(query) = language.variable_query() {
+                let variable_query = TS::Query::new(&parser.language().unwrap(), query).unwrap();
+                self.variables = query_cursor
+                    .matches(&variable_query, root_node, content.as_bytes())
+                    .count();
+            }
+            if let Some(query) = language.loop_query() {
+                let loop_query = TS::Query::new(&parser.language().unwrap(), query).unwrap();
+                self.loops = query_cursor
+                    .matches(&loop_query, root_node, content.as_bytes())
+                    .count();
             }
         }
+
         for line in content.lines() {
             self.total_lines += 1;
             if line.trim().is_empty() {
@@ -97,8 +145,8 @@ impl Stats {
         }
     }
 
-    fn print(&self) {
-        println!("Language: {:?}", self.language);
+    fn print(&self, name: &str) {
+        println!("Language: {:?}", name);
         println!("Total lines: {}", self.total_lines);
         println!("Blank lines: {}", self.blank_lines);
         println!("Functions: {}", self.functions);
@@ -107,25 +155,32 @@ impl Stats {
     }
 }
 
-fn parse_file(language_map: &mut HashMap<Language, Stats>, filename: &str) {
-    let language = Language::new(filename);
-    if let Some(l) = language_map.get_mut(&language) {
-        l.update(filename);
-    } else {
-        language_map.insert(language, Stats::new(filename));
+fn parse_file<'a>(
+    languages: &'a Vec<Box<dyn Language>>,
+    language_map: &mut HashMap<&'a str, Stats>,
+    filename: &str,
+) {
+    for l in languages {
+        if l.matches_filename(filename) {
+            if !language_map.contains_key(l.name()) {
+                language_map.insert(l.name(), Stats::new());
+            }
+            language_map.get_mut(l.name()).unwrap().update(filename, l);
+        }
     }
 }
 
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
-    let mut language_map: HashMap<Language, Stats> = HashMap::new();
+    let mut language_map: HashMap<&str, Stats> = HashMap::new();
+    let languages: Vec<Box<dyn Language>> = vec![Box::new(Rust {}), Box::new(Other {})];
     for arg in args {
         if std::path::Path::new(&arg).is_file() {
-            parse_file(&mut language_map, &arg);
+            parse_file(&languages, &mut language_map, &arg);
         }
     }
 
-    for l in language_map.values() {
-        l.print();
+    for (k, v) in language_map.iter() {
+        v.print(k);
     }
 }
